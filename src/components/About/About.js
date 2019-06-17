f29f6918b9ac9615e26474ad53bed78cab6984d7
AiaRup <aiale88@gmail.com>
2019-06-17 18:45:45 +0200

    add responsive to about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -25,12 +25,12 @@ export default function About() {
           </a>
         </aside>
         <article className="about__text">
-          I have a B.Sc in Biotechnology, but I'm a <span className="about__emphasis">front-end developer</span> with a passion for creating new things. I have the necessary abilities to improvise, grasp new skills and pay attention to the smallest detail. I plan to pursue my career as a front-end
-          developer where I could use the knowledge Iacquired in the 2 front-end courses I did - <span className="about__emphasis">Adalab</span> (in Spain) and <span className="about__emphasis">Google’s Talent Digital</span> (in Israel). I hope to have the opportunity to show my motivation, my
-          curiosity for the programming world and my abilities as an <span className="about__emphasis">autodidact</span>, <span className="about__emphasis">creative</span> and <span className="about__emphasis">team player person</span>.
+          I have a B.Sc in <span className="about__emphasis">Biotechnology</span>, but I'm a front-end <span className="about__emphasis">developer</span> with a passion for creating new things. I have the necessary abilities to improvise, grasp new skills and pay attention to the smallest detail. I
+          plan to pursue my career as a front-end developer where I could use the knowledge Iacquired in the 2 front-end courses I did - <span className="about__emphasis">Adalab</span> (in Spain) and <span className="about__emphasis">Google’s Talent Digital</span> (in Israel). I hope to have the
+          opportunity to show my motivation, my curiosity for the programming world and my abilities as an <span className="about__emphasis">autodidact</span>, <span className="about__emphasis">creative</span> and team player person.
         </article>
       </div>
-      <NavLink smooth to="/AiaRupsom#projects">
+      <NavLink smooth to="/AiaRupsom#projects" className="arrow__link">
         <div className="about__arrow" />
       </NavLink>
     </section>
