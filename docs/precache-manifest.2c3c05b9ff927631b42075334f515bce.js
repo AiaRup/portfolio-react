ccebe2d5c956a1a6c3a7627eb41709e1f418f921
AiaRup <aiale88@gmail.com>
2019-06-18 20:20:32 +0200

    add docs folder

---
+++ docs/precache-manifest.2c3c05b9ff927631b42075334f515bce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3665015715df8043071d61e7de2424a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "257ea0d512794d828223",
+    "url": "/static/css/main.4ccbb9d8.chunk.css"
+  },
+  {
+    "revision": "dcee2960196ebf8730cc",
+    "url": "/static/js/2.b946ca9f.chunk.js"
+  },
+  {
+    "revision": "257ea0d512794d828223",
+    "url": "/static/js/main.fc82e89c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "314651b82ea7e2bc397ac4712db5410c",
+    "url": "/static/media/awesome-profile-cards.314651b8.jpg"
+  },
+  {
+    "revision": "6d3000c5a35b6e59c43fdcab7fc93d72",
+    "url": "/static/media/inherited-code.6d3000c5.jpg"
+  },
+  {
+    "revision": "5ff50519eff602cdc072947a5265d74a",
+    "url": "/static/media/siscode.5ff50519.jpg"
+  },
+  {
+    "revision": "55078b04c7627b6268341b92b6001e06",
+    "url": "/static/media/triplan.55078b04.jpg"
+  },
+  {
+    "revision": "122d5b9fadf130be940fcf2edea9d1d4",
+    "url": "/static/media/weather-chat.122d5b9f.png"
+  }
+]);
